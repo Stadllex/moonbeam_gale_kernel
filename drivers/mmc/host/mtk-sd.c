ccc4df4c6f220d38001d1900968ccdf09c7c3a62
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-03-28 07:58:43 +0700

    unused

--- drivers/mmc/host/mtk-sd.c
+++ drivers/mmc/host/mtk-sd.c
@@ -760,7 +760,7 @@ static void msdc_retry(struct msdc_host *host, int addr, int val, int expr, int 
 static void msdc_reset_hw(struct msdc_host *host)
 {
 	u32 val;
-	u32 count = 0;
+//	u32 count = 0;
 	dev_info(host->mmc->parent, "%s\n",__func__);
 
 	//sdr_set_bits(host->base + MSDC_CFG, MSDC_CFG_RST);
