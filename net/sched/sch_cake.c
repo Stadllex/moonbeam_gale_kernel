3a3dc460f217c68279f46dffd7e6dc194595a10a
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-06-19 14:26:51 +0700

    Adjust CAKE interval and target parameters
    Updated the interval and target values for CAKE.

--- net/sched/sch_cake.c
+++ net/sched/sch_cake.c
@@ -2667,8 +2667,8 @@ static int cake_init(struct Qdisc *sch, struct nlattr *opt,
 
 	q->rate_bps = 0; /* unlimited by default */
 
-	q->interval = 50000; /* 10ms default */
-	q->target   =   4000; /* 5ms: codel RFC argues
+	q->interval = 100000; /* 10ms default */
+	q->target   = 15000; /* 5ms: codel RFC argues
 			       * for 5 to 10% of interval
 			       */
 	q->rate_flags |= CAKE_FLAG_SPLIT_GSO;
